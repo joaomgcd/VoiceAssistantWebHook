47618d69e7831984cfd01b0d094d2b8b8693c4a6
sriramsv <sriramsv1991@gmail.com>
2017-02-11 12:39:18 +0530

    Added timezone

--- assistanthandlers/goodmorning/__init__.py
+++ assistanthandlers/goodmorning/__init__.py
@@ -1,13 +1,12 @@
 from assistanthandlerbase import AssistantHandler
 import urllib
 import arrow
-
+import os
 
 class AssistantHandlerHelloName(AssistantHandler):
 	def __init__(self):
 		AssistantHandler.__init__(self,"goodmorning")
-		self.time=arrow.now()
-
+		self.time=arrow.now(os.getenv("TIMEZONE"))
 	def getBaseUrl(self,parameters):
 		pass
 
@@ -21,10 +20,10 @@ class AssistantHandlerHelloName(AssistantHandler):
 
 	def getSpeech(self, parameters, data):
 		if self.time.hour>0 and self.time.hour<=11:
-			return "good morning"
+			return "good morning sir"
 		elif self.time.hour>=12 and self.time.hour<=16:
-			return "good afternoon"
+			return "good afternoon sir"
 		elif self.time.hour>16 and self.time.hour<=20:
-			return "good evening"
+			return "good evening sir"
 		else:
-			return "good night"
+			return "good night sir"
